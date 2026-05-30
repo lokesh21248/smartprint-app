ef875c8af6176640ec65cafcf88f192653a8ee28
lokesh <shekharramireddy@gmail.com>
2026-05-30 08:05:25 +0530

    Fix SEO sitemap and robots confidfdgvhhkvdftfhhffh ,mdblm;bn gurvdvbrfbation

--- app/order/[shortToken]/page.tsx
+++ app/order/[shortToken]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { 
   CheckCircle2, Clock, Printer, 
